7cdf28a34ab96e539c6c5da1dadd59c65e9fa91a
Santosh Shinde <jshinds4@jci.com>
2021-08-14 20:17:24 +0530

    adding test case1 by tester

---
+++ src/test/java/com/MAVENTEST/TestCase1.java
@@ -0,0 +1,12 @@
+package com.MAVENTEST;
+
+public class TestCase1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("added by tester");
+
+	}
+
+}
